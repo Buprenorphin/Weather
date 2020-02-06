e560f88ae61ad6aa50708cfa814751f909947a65
Roman Maksymchuk <romanmaksymchuk@MacBook-Pro-Roman.local>
2020-02-06 19:18:44 +0200

    weather-site-new-background

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Ternopil&appid=ed6482ee3d073e42650f5b6982083916')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Kanada&appid=ed6482ee3d073e42650f5b6982083916')
     .then(function(resp){return resp.json()})
-    .then(function(date){  
+    .then(function(date){
+
         console.log(date);
+
         document.querySelector('.city-name-text').textContent = date.name;
 
         document.querySelector('.number-text').innerHTML = Math.round(date.main.temp - 273) + '&deg;';
