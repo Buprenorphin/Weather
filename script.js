d027664701f551bb55985a176a36fc136086f9a9
Roman Maksymchuk <romanmaksymchuk@MacBook-Pro-Roman.local>
2020-02-06 19:31:46 +0200

    weather-site-city-list-json

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Kanada&appid=ed6482ee3d073e42650f5b6982083916')
+fetch('https://api.openweathermap.org/data/2.5/weather?q=Ternopil&appid=ed6482ee3d073e42650f5b6982083916')
     .then(function(resp){return resp.json()})
     .then(function(date){
 
